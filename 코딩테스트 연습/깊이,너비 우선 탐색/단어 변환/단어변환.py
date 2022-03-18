da1b0cb3d6eda0860ea22c947dfd85f7cc2e67c0
YoonMinSoo <minsoo9615@naver.com>
2022-03-18 23:22:39 +0900

    03.18

--- 코딩테스트 연습/깊이,너비 우선 탐색/단어 변환/단어변환.py
+++ 코딩테스트 연습/깊이,너비 우선 탐색/단어 변환/단어변환.py
@@ -1,48 +1,36 @@
+from collections import deque
 def solution(begin, target, words):
-    answer = 100
-    sindexs = []
-    eindex = -1
-    for i,word in enumerate(words):
-        bdiff = getdiff(begin,word)
-        if bdiff <= 1:
-            sindexs.append((i,bdiff))
-        if getdiff(target,word) == 0:
-            eindex = i
-    if len(sindexs) == 0 or eindex == -1:
+    answer = 0
+    words.append(begin)
+    if target not in words:
         return 0
+    wdict = {word: i for i, word in enumerate(words)}
+    path = [[] for _ in range(len(words))]
+    for i in range(len(words) - 1):
+        for j in range(i+1, len(words)):
+            w1, w2 = words[i], words[j]
+            diff = 0
+            for a, b in zip(w1, w2):
+                if a != b:
+                    diff += 1
+            if diff == 1:
+                path[i].append(j)
+                path[j].append(i)
     
-    path = getPath(words)
-    cost = getCost(len(words),path)
-    for sindex, bdiff in sindexs:
-        answer = min(answer,cost[sindex][eindex]+bdiff)
-    if answer >= 100:
-        answer = 0
-    return answer
-
-def getPath(words):
-    rt = [[] for i in range(len(words))]
-    for i in range(len(words)):
-        for j in range(i+1,len(words)):
-            if getdiff(words[i],words[j]) == 1:
-                rt[i].append(j)
-                rt[j].append(i)
-    return rt
-                
+    q = deque()
+    cost = [987654321] * len(words)
+    q.append((0, wdict[begin]))
+    while len(q) > 0:
+        ccost, cwidx = q[0]
+        q.popleft()
+        if cost[cwidx] < 987654321:
+            continue
+        cost[cwidx] = ccost
+        for nwidx in path[cwidx]:
+            q.append((ccost + 1, nwidx))
             
-def getdiff(s1, s2):
-    return  sum([1 for a1,a2 in zip(s1,s2) if a1!=a2])
-
-def getCost(length,path):
-    rt = [[100] * length for _ in range(length)] # 100 : intMAX
-    for (i,plist) in enumerate(path):
-        for j in plist:
-            rt[i][j] = 1
-    for k in range(length):
-        for i in range(length):
-            if k == i:
-                continue
-            for j in range(length):
-                if k== j or i ==j:
-                    continue
-                rt[i][j] = min(rt[i][j],rt[i][k]+rt[k][j])
-    return rt
+    if cost[wdict[target]] == 987654321:
+        answer = 0
+    else:
+        answer = cost[wdict[target]]
+    return answer
